f730bb4958a478ec90730696d6fbea16deef04b7
ThorodanBrom <bpr_95@hotmail.com>
2020-03-16 11:18:21 +0530

    Added support for ECDSA certs

--- server/conf/common.py
+++ server/conf/common.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.primitives.serialization import PublicFormat
 
 PEM	= Encoding("PEM")
-PKCS1	= PublicFormat("Raw PKCS#1")
+SubjectPublicKeyInfo	= PublicFormat("X.509 subjectPublicKeyInfo with PKCS#1")
 
 CA_NAME		= "IUDX"
 CA_NAME_LOWER   = CA_NAME.lower()  
